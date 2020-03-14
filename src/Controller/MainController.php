ffa0f19da6eb1b68c3017256243ea1eb0cbd7257
Diego Banos <diegobanosfarinas@gmail.com>
2020-03-14 20:46:15 +0100

    Setup testing

---
+++ src/Controller/MainController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController
+{
+    public function index()
+    {
+        return new Response('Hello World');
+    }
+}
---
+++ tests/Controller/MainControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client  = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
